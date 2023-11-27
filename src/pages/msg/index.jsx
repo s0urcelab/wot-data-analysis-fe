5d24effb82d1e59e7d13b1c1479734656098f566
s0urce <me@src.moe>
2023-11-27 14:02:31 +0800

    feat: Artalk upgrade to 2.6.4

--- src/pages/msg/index.jsx
+++ src/pages/msg/index.jsx
@@ -1,24 +1,22 @@
-import { useState, useLayoutEffect } from 'react'
+import { useState, useLayoutEffect } from 'react';
 import { Tag, Card, Comment, Tooltip, List } from 'antd';
-import Artalk from 'artalk'
-import 'artalk/dist/Artalk.css'
 import './index.less';
 
 function MessageBoard() {
   useLayoutEffect(() => {
     Artalk.init({
-      el:        '#artalk-comments',
-      server:    'https://artalk.src.moe/',
-      site:      '坦克世界360国服数据站',
-      darkMode:  false,
-    })
-  }, [])
+      el: '#artalk-comments',
+      server: 'https://artalk.src.moe/',
+      site: '坦克世界360国服数据站',
+      darkMode: false,
+    });
+  }, []);
 
   return (
     <Card className="comment-card-container">
       <div id="artalk-comments"></div>
     </Card>
-  )
+  );
 }
 
 export default MessageBoard;
