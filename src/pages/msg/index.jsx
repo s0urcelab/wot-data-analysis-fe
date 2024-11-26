4638dfd0d261da8fb8fec86a3eb83710458f12e8
s0urce <me@src.moe>
2024-11-26 12:33:12 +0800

    chore: update artalk

--- src/pages/msg/index.jsx
+++ src/pages/msg/index.jsx
@@ -3,20 +3,20 @@ import { Tag, Card, Comment, Tooltip, List } from 'antd';
 import './index.less';
 
 function MessageBoard() {
-  useLayoutEffect(() => {
-    Artalk.init({
-      el: '#artalk-comments',
-      server: 'https://artalk.src.moe/',
-      site: '坦克世界360国服数据站',
-      darkMode: false,
-    });
-  }, []);
+    useLayoutEffect(() => {
+        Artalk.init({
+            el: '#artalk-comments',
+            server: 'https://artalk.src.moe/',
+            site: '坦克世界360国服数据站',
+            darkMode: false,
+        });
+    }, []);
 
-  return (
-    <Card className="comment-card-container">
-      <div id="artalk-comments"></div>
-    </Card>
-  );
+    return (
+        <Card className="comment-card-container">
+            <div id="artalk-comments"></div>
+        </Card>
+    );
 }
 
 export default MessageBoard;
